d1b1739379f2768d3dbd9a4fb5d5a579d7d8c800
Acosta-Jhon <jhonerick2018@gmail.com>
2021-02-01 23:42:14 -0500

    commit

--- arcade-game/App.js
+++ arcade-game/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola mundo!</Text>
+      <Text>hola mundoooooooo!</Text>
       <StatusBar style="auto" />
     </View>
   );

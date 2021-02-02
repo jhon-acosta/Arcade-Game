f3b0dc913f6919273f9c85616cdef9e141a3b9c5
churitos44 <dany_sanchez44@outlook.com>
2021-02-01 23:58:27 -0500

    segundo cambio

--- arcade-game/App.js
+++ arcade-game/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola mundoooooooo!</Text>
+      <Text>adios mundoooooooo!</Text>
       <StatusBar style="auto" />
     </View>
   );

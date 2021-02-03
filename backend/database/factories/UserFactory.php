74a28a0d2d51762a8b67502bd51011242dc0549b
AndyIsraelYanez <camilo.casas200@gmail.com>
2021-02-02 22:13:15 -0500

    cambios

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace Database\Factories;
+//aumentado
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 class UserFactory extends Factory
 {
@@ -23,7 +24,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+
             'name' => $this->faker->name,
+            'lastname' => $this->faker->sentence(),
+            'age' => $this->faker->sentence(),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password

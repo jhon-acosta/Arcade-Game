588858cf1c889890e49fed36a27d356095b72ca5
Acosta-Jhon <jhonerick2018@gmail.com>
2021-03-07 00:43:14 -0500

    global score

--- arcade-game/screens/views/GlobalScore.js
+++ arcade-game/screens/views/GlobalScore.js
@@ -4,63 +4,76 @@ import tailwind from 'tailwind-rn';
 import { DataTable } from 'react-native-paper';
 import axios from "axios";
 
-
+const API = 'https://arcade-game-v2.herokuapp.com/api'
 
 const GlobalScore = () => {
 
-    const [users, setUser] = useState([])
-    const [departures, setDepartures] = useState([])
+    const [topJud, setTopJud] = useState({
+        user_id: '',
+        nickname: '',
+        time: ''
+    })
 
-    const getUsers = async () => {
-        await axios.get(`${API}/users`)
-            .then(users => {
-                const allUsers = users.data.data;
-                setUser(allUsers);
-                console.log(allUsers)
+    const [topCondorFly, setTopCondorFly] = useState({
+        user_id: '',
+        nickname: '',
+        time: ''
+    })
+
+    const topOneJud = async () => {
+        await axios.get(`${API}/time/min`)
+            .then(res => {
+                const top = res.data;
+                setTopJud(top)
             });
     }
 
-    const getDepartures = async () => {
-        await axios.get(`${API}/departures`)
-            .then(departure => {
-                const allDepartures = departure.data.data;
-                setDepartures(allDepartures);
-                console.log(allDepartures)
+    const topOneCondorFly = async () => {
+        await axios.get(`${API}/point/max`)
+            .then(users => {
+                const top = users.data;
+                setTopCondorFly(top)
             });
     }
 
     useEffect(() => {
-        getUsers()
-        getDepartures()
+        topOneJud()
+        topOneCondorFly()
     }, [])
     return (
-        <View>
-            <Text>JUD</Text>
-            <Text>CONDOR FLY</Text>
-            <DataTable>
-                <DataTable.Header>
-                    <DataTable.Title>Usuario</DataTable.Title>
-                    <DataTable.Title numeric>Apodo</DataTable.Title>
-                    <DataTable.Title numeric>Puntaje</DataTable.Title>
-                </DataTable.Header>
-
-
-                {
-                    departures.map(departure => (
-                        <DataTable.Row key={departures.id}>
-                            <DataTable.Cell >{departure.user_id}</DataTable.Cell>
-                            <DataTable.Cell >{departure.user_id}</DataTable.Cell>
-                            <DataTable.Cell >{departure.score}</DataTable.Cell>
-                        </DataTable.Row>
-                    ))
-                }
-            </DataTable>
+        <View style={styles.container}>
+            <View>
+            <Text style={{fontSize:30, textAlign:'center', padding:25}}>JUD</Text>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>Nickname</DataTable.Title>
+                        <DataTable.Title>Tiempo minimo</DataTable.Title>
+                    </DataTable.Header>
+                    <DataTable.Row key={topJud.user_id}>
+                        <DataTable.Cell >{topJud.nickname}</DataTable.Cell>
+                        <DataTable.Cell >{topJud.time}</DataTable.Cell>
+                    </DataTable.Row>
+                </DataTable>
+            </View>
+            <View>
+                <Text style={{fontSize:30, textAlign:'center', padding:25}}>CONDOR FLY</Text>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>Nickname</DataTable.Title>
+                        <DataTable.Title>Mayor puntaje</DataTable.Title>
+                    </DataTable.Header>
+                    <DataTable.Row key={topCondorFly.user_id}>
+                        <DataTable.Cell >{topCondorFly.nickname}</DataTable.Cell>
+                        <DataTable.Cell >{topCondorFly.point}</DataTable.Cell>
+                    </DataTable.Row>
+                </DataTable>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: tailwind('h-full justify-center items-center'),
+    container: tailwind('h-full justify-center bg-blue-100'),
     textHeader: tailwind('bg-green-600 px-5 py-4 text-lg text-white font-bold')
 });
 

e038ba4af364ae78003b48053f95c54f634306f2
Acosta-Jhon <jhonerick2018@gmail.com>
2021-03-08 11:57:09 -0500

    query

--- backend/app/Http/Controllers/ScoreController.php
+++ backend/app/Http/Controllers/ScoreController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Resources\ScoreResource;
+use App\Models\Departure;
+use App\Http\Resources\DepartureResource;
 
 class ScoreController extends Controller
 {
     public function departureUser($user_id){
-        $user = DB::table('departures')->where('user_id', $user_id)->first();
-        return new ScoreResource($user);
+        // $user = DB::table('departures')->where('user_id', $user_id)->first();
+        $departures = Departure::where('user_id', $user_id)->get();
+        return response()->json($departures);
+        // return new ScoreResource($user);
     }
 
     public function pointMax(Request $request){
